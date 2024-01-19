1f1bf16d02f61ce19ff16cd7a996393b2e2c7474
ItaloCobains <italobrandao.contato@gmail.com>
2024-01-18 22:52:07 -0300

    Add tweets functionality to the application

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,13 @@
+class TweetsController < ApplicationController
+  def create
+    tweet = Tweet.new(tweet_params.merge(user: current_user))
+
+    return unless tweet.save
+
+    redirect_to root_path
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
+end
